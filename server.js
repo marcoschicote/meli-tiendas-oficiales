f5994376ff23d6a33907af469edcdd48445b24a8
Marcos Chicote <totochicote@gmail.com>
2014-11-28 18:20:36 -0300

    Working

---
+++ server.js
@@ -0,0 +1,49 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var async = require('async');
+
+findSellerId = function(elem, callback) {
+	var name = elem.name
+	var storeUrl = elem.url
+
+	request(storeUrl, function(error, response, html) {		
+		var jQuery = cheerio.load(html);					
+		var dataSrc = jQuery("img.brand-logo").attr('src');
+		var slashIndex = dataSrc.lastIndexOf('/');
+
+		var afterSlash = dataSrc.substring(slashIndex + 1);
+		var bkgIndex = afterSlash.indexOf('-');
+
+		var sellerId = afterSlash.substring(0, bkgIndex);
+
+		var e = {name: name, sellerId: sellerId};
+		
+		callback(null, e);
+	});
+}
+
+
+var url = 'http://www.mercadolibre.com.ar/tiendas-oficiales';
+
+
+request(url, function(error, response, html) {
+    if(!error) {
+        var $ = cheerio.load(html);
+
+		var tiendasOficiales = [];
+		$("div.brand-label.short-label.off a").map(function() { 
+													tiendasOficiales.push({ 
+															  name: $(this).attr('title')
+															, url: $(this).attr('href')
+														});
+												});
+
+		async.map(
+			tiendasOficiales
+			, findSellerId
+			, function(err, results) {
+				console.log(results);	
+			}
+		)
+	}
+});
